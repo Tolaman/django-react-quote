801b6c497159715ddd57fed23d347659153fc2ab
Michee Luzayisu <michemakaya11@gmail.com>
2025-03-23 14:36:06 +0200

    corrected: create database model of tow fields

--- core/models.py
+++ core/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 
 class React(models.Model):
-    name = models.CharFiled(max_length=30)
-    detail = model.CharField(max_length=500)
+    name = models.CharField(max_length=30)
+    detail = models.CharField(max_length=500)
